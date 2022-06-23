50d2521a407f2d28a8472d82d94225ab4881b99f
Ricard Roberg <ricardroberg@gmail.com>
2022-06-23 12:11:34 -0300

    Doing class 3

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,13 +1,23 @@
+import { ArrowArcRight, ArrowRight } from "phosphor-react";
+import { useParams } from "react-router-dom";
 import { Header } from "../components/Header";
 import { Sidebar } from "../components/Sidebar";
 import { Video } from "../components/Video";
 
 export function Event() {
+  const { slug } = useParams<{ slug: string }>();
+
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex flex-1">
-        <Video />
+        {slug ? (
+          <Video />
+        ) : (
+          <div className="flex flex-1 text-center text-justify p-10 gap-2 text-2xl bold text-gray-200">
+            Clique em alguma aula <ArrowRight size={24} />
+          </div>
+        )}
         <Sidebar />
       </main>
     </div>
